04de01cd74add785aeb6b61b5adf8392cb337ace
justanormalguy12 <119938147+justanormalguy12@users.noreply.github.com>
2022-12-06 23:22:56 +0100

    Update Script Hub.lua

--- Script Hub.lua
+++ Script Hub.lua
@@ -1,8 +1,9 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("ScriptHub By Quinn", "DarkTheme")
--- Main
+--Main
 local Main = Window:NewTab("Main")
 local MainSection = Main:NewSection("Main")
+
 MainSection:NewButton("Pet Simulator X", "Opens Pet Simulator X Autofarm ", function()
 getgenv().key = "PROJECTWD-Hi1xmiTBtv0gpVAJse"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Main.lua"))()
@@ -13,13 +14,14 @@ loadstring(game:HttpGet("https://raw.githubusercontent.com/ThunderZ-HUB/HUB/main
 end)
 MainSection:NewButton("SHINDO LIFE", "SHINDO LIFE Autofarm", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xiovr/Roblox/main/SL.lua"))()
-end)MainSection:NewButton("KING LEGACY", "KING LEGACY Autofarm", function()
+end)
+MainSection:NewButton("KING LEGACY", "KING LEGACY Autofarm", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xlostpexz/dasdokasdias/Fps/Loading.lua"))()
 end)
 MainSection:NewButton("PROJECT SLAYERS", "PROJECT SLAYERS Autofarm", function()
 repeat wait() until game:IsLoaded() loadstring(game:HttpGet("https://raw.githubusercontent.com/LioK251/RbScripts/main/lazyhub.lua"))()
 end)
---LOCALPLAYER
+--LOCALPlayer
 local Player = Window:NewTab("Player")
 local PlayerSection = Player:NewSection("Player")
 PlayerSection:NewSlider("WalkSpeed", "Speed", 500, 16, function(s) -- 500 (MaxValue) | 0 (MinValue)
@@ -32,8 +34,19 @@ end)
 local Other = Window:NewTab("Other")
 local OtherSection = Other:NewSection("Other")
 OtherSection:NewButton("Troll Menu", "Trololololol", function()
-loadstring(game:HttpGet("https://pastebin.com/raw/kPNgLK28"))()
+loadstring(game:HttpGet("https://pastebin.com/raw/wDh1eTdX"))()
 end)
 OtherSection:NewButton("Chat Spoofer", "spoof ur chat", function()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ant-7802/--/main/straightmilk.lua'))()
+end)
+--Arsenal
+local Arsenal = Window:NewTab("Arsenal")
+local ArsenalSection = Arsenal:NewSection("Arsenal")
+ 
+    ArsenalSection:NewButton("Aimbot", "Right Click to use", function()
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/justanormalguy12/scripthub/main/Aimbot%20any%20game.lua'))()
+    end)
+    
+    ArsenalSection:NewButton("Chams/esp", "Chams", function()
+       loadstring(game:HttpGet('https://raw.githubusercontent.com/justanormalguy12/scripthub/main/chamsglow_0x83.lua'))()
 end)
