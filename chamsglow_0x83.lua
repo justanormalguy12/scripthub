4c9e63c788a2a3f95298755609d1460caa86f3e4
justanormalguy12 <119938147+justanormalguy12@users.noreply.github.com>
2022-12-06 22:39:29 +0100

    Update chamsglow_0x83.lua

--- chamsglow_0x83.lua
+++ chamsglow_0x83.lua
@@ -6,7 +6,7 @@ local settings_tbl = {
     ESP_Enabled = true,
     ESP_TeamCheck = false,
     Chams = true,
-    Chams_Color = Color3.fromRGB(0,0,0),
+    Chams_Color = Color3.fromRGB(255,255,255),
     Chams_Transparency = 0.1,
     Chams_Glow_Color = Color3.fromRGB(255,255,255)
 }
