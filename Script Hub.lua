636ae30b3f30d0f64ff102dac854b5c997a0d329
justanormalguy12 <119938147+justanormalguy12@users.noreply.github.com>
2022-12-07 20:44:40 +0100

    Update Script Hub.lua

--- Script Hub.lua
+++ Script Hub.lua
@@ -58,45 +58,6 @@ local ArsenalSection = Arsenal:NewSection("Arsenal")
         loadstring(game:HttpGet("https://raw.githubusercontent.com/bruvzz/oldinfinitejump/main/script"))()
     end)
 
-    ArsenalSection:NewToggle("ToggleText", "ToggleInfo", function(state)
-    if state then
-       _G.chams = false
-local Players = game:GetService("Players") -- variable to get the players in the game
-
-script.Parent.MouseButton1Click:Connect(function()
-	if _G.chams == false then
-		_G.chams = true
-		--- Chams
-		function CreateGui(name,parent,face) -- function that creates the Chams
-			local SurfaceGui = Instance.new("SurfaceGui",parent) --- Creates a SurfaceGui in the game
-			SurfaceGui.Parent = parent
-			SurfaceGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-			SurfaceGui.Face = Enum.NormalId[face]
-			SurfaceGui.LightInfluence = 0
-			SurfaceGui.ResetOnSpawn = false
-			SurfaceGui.Name = name
-			SurfaceGui.AlwaysOnTop = true
-			local Frame = Instance.new("Frame",SurfaceGui)
-			Frame.BackgroundColor3 = Color3.fromRGB(255, 19, 235) -- colour for the surfacgui
-			Frame.Size = UDim2.new(1,0,1,0)
-		end
-
-		while wait(1) do
-			for i,v in pairs (Players:GetPlayers()) do --- gets all the players in the game and loops through them
-				if v ~= Players.LocalPlayer and v.Character ~= nil and v.Character:FindFirstChild("Head") and _G.chams and v.Character.Head:FindFirstChild("cham") == nil and v.TeamColor ~= Players.LocalPlayer.TeamColor then --- Checks to check if the player is appropiate to make a cham
-					for i,v in pairs (v.Character:GetChildren()) do -- looping through every child in the character of the player
-						if v:IsA("MeshPart") or v.Name == "Head" then -- checking if the child is a body part
-							CreateGui("cham",v,"Back")
-							CreateGui("cham",v,"Front")
-							CreateGui("cham",v,"Top")
-							CreateGui("cham",v,"Bottom")
-							CreateGui("cham",v,"Right")
-							CreateGui("cham",v,"Left")
-						end
-					end
-               end
-		end
-    else
-        _G.chams = false
-    end
+    ArsenalSection:NewButton("Silent Aim", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://pastebin.com/raw/bQtfYNgh"))()
 end)
